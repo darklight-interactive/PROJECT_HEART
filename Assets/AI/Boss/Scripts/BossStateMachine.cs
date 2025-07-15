e3e62440d6a1b6386cde03984877930c3ac383f2
timpt0261 <rchavez0215@gmail.com>
2025-07-15 01:30:13 -0700

    Add initial boss AI, prefab, and state machine
    Introduced a new boss system including prefab, state machine script, boss stats, and animation assets. Added supporting meta files and updated scenes to integrate the new boss. This lays the foundation for boss behavior and interactions in the game.

---
+++ Assets/AI/Boss/Scripts/BossStateMachine.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace ProjectHeart
+{
+    public class BossStateMachine : MonoBehaviour
+    {
+        private enum BOSS_PHASES { START, PHASE_01, PHASE_02, PHASE_03, DEAD }
+
+        [Header("Refrences")]
+        [SerializeField] private GameObject Player;
+
+        [Header("Stats")]
+        [SerializeField] private BossStats bossStats;
+
+        [Header("RigidBody")]
+        [SerializeField] private Rigidbody rb;
+
+        [Header("Animator")] private Animator animator;
+
+        // [Header("UI/UX")] private 
+
+        [Header("Phases")]
+        [SerializeField] private float attackDuration;
+        [SerializeField] private BOSS_PHASES currentPhase;
+
+        public UnityEvent OnHurt;
+        public UnityEvent PHASE_01;
+        public UnityEvent PHASE_02;
+        public UnityEvent PHASE_03;
+
+
+        void Awake()
+        {
+            currentPhase = BOSS_PHASES.START;
+        }
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Check Health
+            // Update to appropriate state
+            UpdateState();
+        }
+
+        void FixedUpdate()
+        {
+
+        }
+
+
+
+        #region State Machine
+
+        public void UpdateState()
+        {
+            switch (currentPhase)
+            {
+                case BOSS_PHASES.PHASE_01:
+                    PHASE_01?.Invoke();
+                    break;
+                case BOSS_PHASES.PHASE_02:
+                    PHASE_02?.Invoke();
+                    break;
+                case BOSS_PHASES.PHASE_03:
+                    PHASE_03?.Invoke();
+                    break;
+            }
+        }
+    
+
+        public void OnGameStateChanged()
+        {
+
+        }
+        #endregion
+
+        // #region Animation Triggers
+
+        // #endregion
+
+        // #region Movement
+        // #endregion
+    }
+}
